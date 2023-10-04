876e2a3ddf6a0c5282cf88ac8b1f9d26b56974ca
Ali Erdem Sunar <alierdem.sunar@pinet.com.tr>
2023-10-04 18:13:39 +0300

    Initial commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Panther\Client;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return 'home';
+        $client = Client::class::createChromeClient();
+// Or, if you care about the open web and prefer to use Firefox
+        $client = Client::createFirefoxClient();
+
+        $client->request('GET', 'https://api-platform.com'); // Yes, this website is 100% written in JavaScript
+        $client->clickLink('Getting started');
+
+// Wait for an element to be present in the DOM (even if hidden)
+        $crawler = $client->waitFor('#installing-the-framework');
+// Alternatively, wait for an element to be visible
+        $crawler = $client->waitForVisibility('#installing-the-framework');
+
+        echo $crawler->filter('#installing-the-framework')->text();
+        $client->takeScreenshot('screen.png'); // Yeah, screenshot!
+        //
     }
 }
